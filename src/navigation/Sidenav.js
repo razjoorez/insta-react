c63f713f62ea2e2370b28ecc11a5a936c612035a
reza razjoo <reza.razjoo@yahoo.com>
2023-10-10 21:46:33 +0100

    mui icons added

--- src/navigation/Sidenav.js
+++ src/navigation/Sidenav.js
@@ -3,6 +3,11 @@ import './Sidenav.css';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import ExploreIcon from '@mui/icons-material/Explore';
+import SlideshowIcon from '@mui/icons-material/Slideshow';
+import ChatIcon from '@mui/icons-material/Chat';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Sidenav() {
   return (
@@ -11,19 +16,35 @@ function Sidenav() {
             instaReact
         </div>
         <div className='sidenav__buttons'>
-            <div className='side__button'>
+            <button className='sidenav__button'>
                 <HomeIcon/>
                 <span>Home</span>
-            </div>
-            <div className='side__button'>
+            </button>
+            <button className='sidenav__button'>
                 <SearchIcon/>
                 <span>Search</span>
 
-            </div>
-            <div className='side__button'>
+            </button>
+            <button className='sidenav__button'>
                 <ExploreIcon/>
                 <span>Explore</span>
-            </div>
+            </button>
+            <button className='sidenav__button'>
+                <SlideshowIcon/>
+                <span>Reels</span>
+            </button>
+            <button className='sidenav__button'>
+                <ChatIcon/>
+                <span>Messages</span>
+            </button>
+            <button className='sidenav__button'>
+                <FavoriteBorderIcon/>
+                <span>Notifications</span>
+            </button>
+            <button className='sidenav__button'>
+                <AddCircleOutlineIcon/>
+                <span>Create</span>
+            </button>
         </div>
     </div>
   )
